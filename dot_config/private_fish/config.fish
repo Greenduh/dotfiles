1f90777afe45103ea5dcb929ed539ec52d052203
greend <greendsmain@gmail.com>
2026-03-19 01:47:40 +0200

    random bullshit

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,12 @@
 if status is-interactive
     set -g fish_greeting
-    source ~/my-environment.sh
     zoxide init fish | source
     starship init fish | source
 
     alias hx='helix'
     alias rmorphan='yay -Qdtq | yay -Rns -'
     alias softreboot='systemctl soft-reboot'
-    alias wttr='curl v2d.wttr.in'
 
-    fastfetch
+    alias cd='z'
+    alias ls='eza --icons=auto --group-directories-first'
 end
